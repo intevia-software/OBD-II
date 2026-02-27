96ad537dc345f195f87c384ea1eebd5c13fcf734
lakhdar-195 <kodlak.developpement@gmail.com>
2026-02-27 18:16:16 +0100

    add default

--- src/components/Readef.jsx
+++ src/components/Readef.jsx
@@ -6,31 +6,47 @@ function Readef() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // fetch("http://localhost:5000/api/get/default") // ton endpoint Python
-    //   .then((res) => res.json())
-    //   .then((data) => {
-    //     setCodes(data);
-    //     setLoading(false);
-    //   })
-    //   .catch((err) => {
-    //     console.error("Erreur récupération OBD :", err);
-    //     setLoading(false);
-    //   });
+    
+        fetch("http://localhost:5000/api/get/default") // ton endpoint Python
+        .then((res) => res.json())
+        .then((data) => {
+            setCodes(data);
+            setLoading(false);
+        })
+        .catch((err) => {
+            console.error("Erreur récupération OBD :", err);
+            setLoading(false);
+        });
 
-    const data = ["P0001", "P0003", "P0004", "P0005", "P0006", "P0007", "P0008", "P0009", "P0010"]
-    setCodes(data);
-    setLoading(false);
     
   }, []);
 
-  if (loading) return <p>Chargement...</p>;
+  if (loading) return (
+
+        <div className="w-full py-10">
+
+            <div className="w-full p-3 bg-gray-600 text-center rounded">
+                <h2 className="text-gray-300 text-lg">Chargement...</h2>
+            </div>
+        </div>
+    
+    );
 
   if (codes.length === 0) {
-    return <p>Aucun défaut détecté ✅</p>;
+    return (
+
+        <div className="w-full py-10">
+
+            <div className="w-full p-3 bg-gray-600 text-center rounded">
+                <h2 className="text-gray-300 text-lg">Aucun défaut détecté ✅</h2>
+            </div>
+        </div>
+    
+    );
   }
 
   return (
-    <div className="w-full p-10">
+    <div className="w-full py-10">
 
         <div className="w-full p-3 bg-gray-600 text-center rounded">
             <h2 className="text-gray-300 text-lg">Codes défaut détectés :</h2>
